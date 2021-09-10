065a5583b6d7092b31752c00fac7a515216d0df5
Charley1013 <79081800+jiho3894@users.noreply.github.com>
2021-09-10 20:44:38 +0900

    Add files via upload

--- 2week/Submit.java
+++ 2week/Submit.java
@@ -1,29 +1,28 @@
 package testhava02;
-
 import java.util.Scanner;
 
 public class Submit {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("점수를 입력하시오:");
-		Scanner input = new Scanner(System.in); // new Scanner
-		// while문을 이용한 무한 점수 확인
-		while(true) {
-			int score = input.nextInt(); 
-			// 삼한 연산자를 이용한 점수별 등급 
-			char grade = (score >= 90) ? 'A' : 
-				((score >= 80) ? 'B' : 
-					((score >= 70) ? 'C' : 
-						((score >= 60) ? 'D' : 
-							'F')));
-			// loop를 끊기위한 특정 int 값 입력시 break 발동
-			if(score == 0) {
-				System.out.println("종료");
-				break;
-			} 
-			System.out.println(score + "점은" + grade + "등급입니다."); 
-			System.out.println("점수를 입력하시오:");
-		}
-	} 
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in); // new Scanner
+      System.out.println("점수를 입력하세요:");
+      // while문을 이용한 무한 점수 확인
+      while(true) {
+         String finish = input.nextLine();
+         if(finish.equals("q")) {
+            System.out.println("종료");
+            break;
+      } 
+      // string to int parse
+      int score = Integer.parseInt(finish);
+      // 삼항연산자로 점수별 등급 표기
+      char grade = (score >= 90) ? 'A' : 
+         ((score >= 80) ? 'B' : 
+            ((score >= 70) ? 'C' : 
+               ((score >= 60) ? 'D' : 
+                  'F')));
+      System.out.println(score + "점은" + grade + "등급입니다."); 
+      System.out.println("종료는 q를 입력해주세요");
+      System.out.println("점수 입력 : "); 
+      } 
+   }
 }
