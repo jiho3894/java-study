2377480b0bef250522d0eba4a170338358c9a1ec
Charley1013 <79081800+jiho3894@users.noreply.github.com>
2021-09-14 20:14:53 +0900

    Add files via upload

---
+++ week3/ForEx01.java
@@ -0,0 +1,15 @@
+package testjava03;
+
+public class ForEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		
+		for(int i=1; i<=100; i++) {
+			sum += i;
+		}
+		System.out.println("1~100 ÀÇ ÇÕ" + sum);
+	}
+	
+}
