7502f0c0e9f7213328f6fd27cbb6fba9b585414d
Charley1013 <79081800+jiho3894@users.noreply.github.com>
2021-12-16 13:20:13 +0900

    Delete week3-3 directory

--- week3-3/ThreadB.java
+++
@@ -1,9 +0,0 @@
-package threadEx01;
-
-public class ThreadB extends Thread{
-   public void run() {
-      for(int i=0; i<2; i++) {
-         System.out.println(getName() + "가 출력한 내용");
-      }
-   }
-}
